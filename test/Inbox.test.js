f3162a0a308f1f8d46634b5f3d5b75ce6ed8dc59
kanbaknurullah <kanbaknurullah@gmail.com>
2022-04-29 10:54:52 +0300

    First commit

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -20,7 +20,12 @@ beforeEach( async () => {
 
 describe ('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(inbox);
+        assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hi there!');
     });
 });
 
